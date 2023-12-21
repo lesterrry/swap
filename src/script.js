5d17ccfc125c729e351fe89e574e7c86e3cf81ff
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-12-22 00:07:37 +0300

    initial js

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ me@aydar.media
 import './style.css'
 
 import { gsap } from 'gsap';
+import * as three from 'three'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
 
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { SplitText } from './split_text';
@@ -23,6 +25,43 @@ const paragraphs = [
     'section.buy p:nth-of-type(1)',
     'section.subscribe p:nth-of-type(1)',
 ]
+
+let swap
+const canvas = document.querySelector('.logo canvas')
+const sizes = {
+	width: canvas.clientWidth,
+	height: 3100
+}
+let cursor = {
+	x: 0,
+	y: 0
+}
+
+const clock = new three.Clock()
+const camera = new three.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const scene = new three.Scene()
+const renderer = new three.WebGLRenderer({
+	canvas: canvas,
+	antialias: true,
+	alpha: true
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+})
+
+window.addEventListener('resize', () => {
+	sizes.width = canvas.clientWidth
+	// sizes.height = canvas.clientHeight
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 window.addEventListener('load', () => {
     for (const i of paragraphs) {
@@ -42,16 +81,43 @@ window.addEventListener('load', () => {
         });
     }
 
-    // const scrollAnimation2 = gsap.to('section.description p:nth-of-type(2)', {
-    //     color: 'white',
-    //     scrollTrigger: {
-    //         trigger: 'section.description',
-    //         start: 'top 65%',
-    //         end: 'bottom 35%',
-    //         // markers: true,
-    //         toggleActions: 'restart none none reverse',
-    //         duration: 0.5
-    //     },
-    // });
+    console.log(sizes);
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    camera.position.set(6, -2, 5)
+	scene.add(camera)
+
+	const hemiLight = new three.HemisphereLight(0xFFFFFF, 0x444444, 1)
+	hemiLight.position.set(200, 200, 200)
+	scene.add(hemiLight)
+
+    const loader = new FBXLoader()
+	loader.load('/3d/swap.fbx', (object) => {
+		object.rotation.z = 1
+        object.rotation.y = 2
+        object.traverse((child) => {
+			if (child.isMesh) {
+				child.castShadow = true
+				child.receiveShadow = true
+			}
+		})
+		swap = object
+		scene.add(object)
+	})
+
+    threeTick();
 });
+
+const threeTick = () => {
+	// const elapsed = clock.getElapsedTime()
+
+	// camera.position.x = (cursor.x + 1) / 2
+	// camera.position.y = (cursor.y / 2) + 9
+	// camera.lookAt(new THREE.Vector3(0, 6, 0))
+
+	renderer.render(scene, camera)
+
+	window.requestAnimationFrame(threeTick)
+}
 
